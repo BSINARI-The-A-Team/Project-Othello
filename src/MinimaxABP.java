0f580e65894d9c0cd33c171a7bd60954be5d3244
T.H. Engberg <tore0402@gmail.com>
2026-03-14 13:55:07 +0100

    chore: add initial empty functions for MinimaxABP and pseudocode comment

--- src/MinimaxABP.java
+++ src/MinimaxABP.java
@@ -1,10 +1,53 @@
+import java.util.*;
+ 
 public class MinimaxABP implements IOthelloAI{
 
-	/**
-	 * Add MinimaxABP code here
-	 */
 	public Position decideMove(GameState s){
-		return new Position(-1,-1);
+		/**
+         * Add MinimaxABP functions here
+         * when they are done
+         */
+        return new Position(-1,-1); // placeholder return value
 	}
+
+    public void ALPHA_BETA_SEARCH(GameState s){}
+
+    public void MAX_VALUE(GameState s, int alpha, int beta){}
+
+    public void MIN_VALUE(GameState s, int alpha, int beta){}
+
+    public void UTILITY(GameState s, int currentPlayer){}
+
+    public void IS_TERMINAL(GameState s){}
+
+    public void ACTIONS(GameState s){}
 }
 
+/*
+function ALPHA-BETA-SEARCH(game, state) returns an action
+    player←game.TO-MOVE(state)
+    value, move←MAX-VALUE(game, state,−∞,+∞)
+    return move
+
+function MAX-VALUE(game, state,α, β) returns a (utility, move) pair
+    if game.IS-TERMINAL(state) then return game.UTILITY(state, player), null
+    v←−∞
+    for each a in game.ACTIONS(state) do
+        v2, a2←MIN-VALUE(game, game.RESULT(state, a),α, β)
+        if v2 > v then
+            v, move←v2, a
+            α←MAX(α, v)
+        if v ≥ β then return v, move
+    return v, move
+
+function MIN-VALUE(game, state,α, β) returns a (utility, move) pair
+    if game.IS-TERMINAL(state) then return game.UTILITY(state, player), null
+    v←+∞
+    for each a in game.ACTIONS(state) do
+        v2, a2←MAX-VALUE(game, game.RESULT(state, a),α, β)
+        if v2 < v then
+            v, move←v2, a
+            β←MIN(β, v)
+        if v ≤ α then return v, move
+    return v, move
+*/
